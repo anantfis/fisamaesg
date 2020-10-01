16e4f2c15a727dfd30366e4d83be3cb0ee38733b
Anant Joshi <Anant.Joshi@FISGLOBAL.COM>
2020-10-01 04:13:12 -0400

    Initial Checkin for esg ama

---
+++ Fis_Ama_EsgAnalytics/Fis_Ama_EsgAnalytics/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Fis_Ama_EsgAnalytics
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
