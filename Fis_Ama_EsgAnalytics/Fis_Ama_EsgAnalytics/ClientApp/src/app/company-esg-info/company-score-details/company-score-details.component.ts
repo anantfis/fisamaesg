041d00b0974f021ddd2c3cf9fa19461c242029f1
Mukesh.K.Kumar <Mukesh.K.Kumar@fisglobal.com>
2020-10-07 06:53:55 -0400

    Making Details page more dynamic.

--- Fis_Ama_EsgAnalytics/Fis_Ama_EsgAnalytics/ClientApp/src/app/company-esg-info/company-score-details/company-score-details.component.ts
+++ Fis_Ama_EsgAnalytics/Fis_Ama_EsgAnalytics/ClientApp/src/app/company-esg-info/company-score-details/company-score-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { company } from '../../models/company';
+import { esgInputData } from '../../models/esgInputData';
+import { EsgDataService } from '../../service/esg-data.service';
 
 @Component({
   selector: 'app-company-score-details',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-score-details.component.css']
 })
 export class CompanyScoreDetailsComponent implements OnInit {
-
-  constructor() { }
+  @Input() companySelected: company;
+  companyData: esgInputData;
+  companyInput: company;
+  constructor(private esgDataService: EsgDataService) { }
 
   ngOnInit() {
+    this.companySelected = this.esgDataService.getAllCompanies()[0]; // should come through @Input, remove hardcoding later
+    this.companyInput = this.esgDataService.getAllCompanies().filter(x => x.companyId === this.companySelected.companyId)[0];
+    this.companyData = this.esgDataService.getAllEsgData().filter(y => y.company.companyId === this.companyInput.companyId)[0];
+
+  }
+
+  pickColor(score: number): string {
+    if (score >= 75)
+      return "assets/greendot.png";
+    else if (score <=35)
+      return "assets/reddot.png";
+    if (score > 35 && score < 75)
+      return "assets/yellowdot.png";
   }
 
 }
--- Fis_Ama_EsgAnalytics/Fis_Ama_EsgAnalytics/ClientApp/src/app/compare-esg-info/compare-score-details/compare-score-details.component.ts
+++ Fis_Ama_EsgAnalytics/Fis_Ama_EsgAnalytics/ClientApp/src/app/compare-esg-info/compare-score-details/compare-score-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { EsgDataService } from '../../service/esg-data.service';
+import { company } from '../../models/company';
+import { esgInputData } from '../../models/esgInputData';
 
 @Component({
   selector: 'app-compare-score-details',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompareScoreDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() companiesSelected: company[];
+  constructor(private esgDataService: EsgDataService) { }
+  companyInput1: company;
+  companyInput2: company;
+  companyInput3: company;
+  companyData1: esgInputData;
+  companyData2: esgInputData;
+  companyData3: esgInputData;
 
   ngOnInit() {
+    // should come through @Input, remove hardcoding later
+    this.companyInput1 = this.esgDataService.getAllCompanies()[0];//this.companiesSelected[0]
+    this.companyInput2 = this.esgDataService.getAllCompanies()[1];//this.companiesSelected[1]
+    this.companyInput3 = this.esgDataService.getAllCompanies()[2];//this.companiesSelected[2]
+
+    this.companyData1 = this.esgDataService.getAllEsgData().filter(y => y.company.companyId === this.companyInput1.companyId)[0];
+    this.companyData2 = this.esgDataService.getAllEsgData().filter(y => y.company.companyId === this.companyInput2.companyId)[0];
+    this.companyData3 = this.esgDataService.getAllEsgData().filter(y => y.company.companyId === this.companyInput3.companyId)[0];
+
+   //this.companyData3.company.companyName
+   // this.companyData3.goalBasedScore.goal1
+  }
+
+  pickColor(score: number): string {
+    if (score >= 75)
+      return "assets/greendot.png";
+    else if (score <= 35)
+      return "assets/reddot.png";
+    if (score > 35 && score < 75)
+      return "assets/yellowdot.png";
+  }
+  pickRating(score: number): string {
+    if (score >= 75)
+      return "Strong";
+    else if (score <= 35)
+      return "Weak";
+    if (score > 35 && score < 75)
+      return "Average";
   }
 
 }
