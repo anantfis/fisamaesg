f32536b2eb7ad396fe1993daf17a778d0f9332a7
Mukesh.K.Kumar <Mukesh.K.Kumar@fisglobal.com>
2020-10-13 03:40:10 -0400

    fix to include multiple providers

--- Fis_Ama_EsgAnalytics/Fis_Ama_EsgAnalytics/ClientApp/src/app/models/sector-company-basedata.ts
+++ Fis_Ama_EsgAnalytics/Fis_Ama_EsgAnalytics/ClientApp/src/app/models/sector-company-basedata.ts
@@ -1,14 +1,15 @@
 import { sector, sectorGoalWeightage } from "./sector";
 import { company } from "./company";
 import { esgScore } from "./score";
+import { provider } from "./provider";
 
 export class SectorCompanyParameterBaseData {
   constructor(sector: sector,
     company: company,
     totalEsgScore: number,
-    e_sector_region_score: number,
-    s_sector_region_score: number,
-    g_sector_region_score: number,
+    e_sector_region_score: number[],
+    s_sector_region_score: number[],
+    g_sector_region_score: number[],
     manufacturingParametersRating: ParametersRating[],
     pharmaParametersRatings: ParametersRating[],
     softwareParametersRatings: ParametersRating[]) {
@@ -21,14 +22,13 @@ export class SectorCompanyParameterBaseData {
     manufacturingParametersRating: manufacturingParametersRating;
     pharmaParametersRatings: pharmaParametersRatings;
     softwareParametersRatings: softwareParametersRatings;
-
   }
   sector: sector;
   company: company;
   totalEsgScore: number;
-  e_sector_region_score: number;
-  s_sector_region_score: number;
-  g_sector_region_score: number;
+  e_sector_region_score: number[];
+  s_sector_region_score: number[];
+  g_sector_region_score: number[];
   manufacturingParametersRating: ParametersRating[];
   pharmaParametersRatings: ParametersRating[];
   softwareParametersRatings: ParametersRating[];
@@ -37,6 +37,7 @@ export class SectorCompanyParameterBaseData {
 export class ParametersRating {
   sectorGoalWeightage: sectorGoalWeightage;
   esgScore: esgScore;
+  provider: provider;
 }
 
 
